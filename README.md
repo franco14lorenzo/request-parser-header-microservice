646d71de5414a3ece18ef9e96253815355712ad6
franco14lorenzo <franco14lorenzo@gmail.com>
2021-08-22 22:24:06 -0300

    furst commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# REQUEST HEADER PARSER MICROSERVICE
+
+Este es el proyecto de un microservicio el cual sirve datos del usuario, es el segundo de cinco proyectos requeridos por freecodecamp.org para obtener el certificado de APIs y microservicios.
+
+https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice
+
+DEMO:
+
+![Request Header Parser Microservice](https://i.ibb.co/w4Pr6DF/request-header-parser-microservice.png)
---
+++ server.js
@@ -0,0 +1,34 @@
+
+require('dotenv').config();
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+app.get("/api/whoami", function (req, res) {
+  let userIp = req.socket.remoteAddress
+  let userLanguage = req.headers["accept-language"]
+  let userOS = req.headers["user-agent"]
+
+  res.json({"ipaddress": userIp, "language": userLanguage, "software": userOS})
+
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
